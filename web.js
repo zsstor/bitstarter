2558dabefc0506e70ed6b91e5c03ec4b25f138db
Zachary Starer-Stor <zsstor@gmail.com>
2013-07-09 22:12:02 -0400

    what

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+var fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buf = fs.readFileSync('index.html');
+var str = buf.toString();
+console.log("Echo");
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
